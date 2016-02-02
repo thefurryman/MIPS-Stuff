7a636466161286d6bcee69e509793d0b67f3547e
thefurryman <tofumaster536@gmail.com>
2016-02-01 23:07:25 -0800

    added comments

--- MIPS swap 2 arrays.asm
+++ MIPS swap 2 arrays.asm
@@ -8,32 +8,34 @@ space: .asciiz " "
 .globl main
 .text
 
-main:
+main: #load arrays and sizes
 la $a0, array1
 la $a1, array2
-li $t0, 80
-li $t1, 0
+li $t0, 80 #array counter 20 * 4 = 80
+li $t1, 0 #$t1 and $t2 for starting byte indexes for array
 li $t2, 0
 
 swap:
 beqz $t0, exit
 
-lw $t3, 0($a0)
+lw $t3, 0($a0) #load indexes from array1 and array2 at $a0 and $a1 respectively
 lw $t4, 0($a1)
 
-move $t5, $t3 #store $t3 temp
+#swapping values from same index
+move $t5, $t3 #store $t3 temp 
 move $t3, $t4 #array2 to array1
-sw $t3, 0($a0)
+sw $t3, 0($a0) #swapping actual values
 move $t4, $t5 #array1 temp to array2
 sw $t4, 0($a1)
 
-addi $a0, $a0, 4
-addi $a1, $a1, 4
-addi $t0, $t0, -4
+addi $a0, $a0, 4 #offset array1 to next integer
+addi $a1, $a1, 4 #offset array2 to next integer
+addi $t0, $t0, -4 #offset the counter by -4
 
-j swap
+j swap 
 
 exit:
+#loads only array1 to test what is stored and print
 la $t1, array1
 li $t7, 80
 
@@ -41,7 +43,7 @@ jal print
 li $v0, 10
 syscall
 
-print:
+print: #loop to print array1
 beqz $t7, tran
 
 li $v0, 1
@@ -49,14 +51,15 @@ lw $t2, 0($t1)
 move $a0, $t2
 syscall
 
-li $v0, 4
+#add a space in between numbers
+li $v0, 4 
 la $a0, space
 syscall
 
-addi $t1, $t1, 4
-addi $t7, $t7, -4
+addi $t1, $t1, 4 #offset array index to loop through
+addi $t7, $t7, -4 #offset counter to end loop
 
 j print
 
-tran:
+tran: #jump back to after jal print
 jr $ra
