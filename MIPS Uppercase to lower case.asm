6d28a5c32309f68999e61f0b47de63e34ea88dd5
thefurryman <tofumaster536@gmail.com>
2016-02-01 23:03:13 -0800

    added comments at store byte at line 21
    sb to actually swap values

--- MIPS Uppercase to lower case.asm
+++ MIPS Uppercase to lower case.asm
@@ -7,25 +7,26 @@ newLine: .asciiz "\n"
 .globl main
 .text
 
-main:
+main: #load size and string
 la $a0, size
 lw  $a0, 0($a0)
 la $a1, string
-move $t0, $a0
+move $t0, $a0 #move size to $t0 register for counter
 
 check:
-beqz $t0, exit
+beqz $t0, exit #when size = 0, exit
 
-lb $t1, 0($a1)
-addi $t1, $t1, 32
-addi $t0, $t0, -1
-addi $a1, $a1, 1
+lb $t1, 0($a1) #load byte at $a1
+addi $t1, $t1, 32 #convert to lower case add 32 (decimal ascii)
+sb $t1, 0($a1) #UNTESTED, switches original uppercase with new lowercase character
+addi $t0, $t0, -1 #-1 offset counter for size
+addi $a1, $a1, 1 #offset byte location on string to the next character
 
-li $v0, 11
+li $v0, 11 #print character
 move $a0, $t1
 syscall
 
-j check
+j check #jump back to check function to continue loop
 
 exit:
 li $v0, 10
